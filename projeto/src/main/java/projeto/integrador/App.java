fae5c55f3ee2d6d966d72a292236ac23b539ad8a
Emilly-Freire <emillyfreireluna17@gmail.com>
2025-11-14 08:52:53 -0300

    criando arquivos e pastas para começo do projeto

--- projeto/src/main/java/projeto/integrador/App.java
+++ projeto/src/main/java/projeto/integrador/App.java
@@ -1,13 +1,17 @@
 package projeto.integrador;
 
+import projeto.integrador.controllers.IndexController;
+import projeto.integrador.utils.JavalinUtils;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ) {
+        var app = JavalinUtils.makeApp(7072);
+        IndexController indexController = new IndexController();
+
+        app.get("/", indexController.get);
     }
 }
