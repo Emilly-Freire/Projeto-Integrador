d0ed33ceb067f9def86e1b334fb5ba9d982bd996
Emilly-Freire <emillyfreireluna17@gmail.com>
2025-11-28 10:35:21 -0300

    implementando o CadastroDisciplinaController

--- projeto/src/main/java/projeto/integrador/controllers/CadastroDisciplinaController.java
+++ projeto/src/main/java/projeto/integrador/controllers/CadastroDisciplinaController.java
@@ -1,6 +1,22 @@
 package projeto.integrador.controllers;
 
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
+import projeto.integrador.model.repository.DisciplinaRepository;
+
 public class CadastroDisciplinaController {
     
+    private DisciplinaRepository repositorio;
+
+    public CadastroDisciplinaController(DisciplinaRepository repositorio) {
+        this.repositorio = repositorio;
+    }
     
+    public Handler get = (Context ctx)->{
+        ctx.render("diciplinas.html");
+    };
+
+    public Handler post = (Context ctx)->{
+        String nome = ctx.formParam("nome");
+    };
 }
