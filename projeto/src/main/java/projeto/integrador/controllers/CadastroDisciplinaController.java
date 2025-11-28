f0fd41d5e4ca057f6292196a15e545754d868375
Emilly-Freire <emillyfreireluna17@gmail.com>
2025-11-28 11:36:07 -0300

    atualizando CadastroDisciplinaController

--- projeto/src/main/java/projeto/integrador/controllers/CadastroDisciplinaController.java
+++ projeto/src/main/java/projeto/integrador/controllers/CadastroDisciplinaController.java
@@ -1,10 +1,7 @@
 package projeto.integrador.controllers;
 
-<<<<<<< HEAD
-=======
 import io.javalin.http.Context;
 import io.javalin.http.Handler;
->>>>>>> d0ed33ceb067f9def86e1b334fb5ba9d982bd996
 import projeto.integrador.model.repository.DisciplinaRepository;
 
 public class CadastroDisciplinaController {
@@ -14,12 +11,6 @@ public class CadastroDisciplinaController {
     public CadastroDisciplinaController(DisciplinaRepository repositorio) {
         this.repositorio = repositorio;
     }
-<<<<<<< HEAD
-
-    /*public Handler get = (Context ctx)->{
-        ctx.render("");
-    }*/
-=======
     
     public Handler get = (Context ctx)->{
         ctx.render("diciplinas.html");
@@ -28,5 +19,4 @@ public class CadastroDisciplinaController {
     public Handler post = (Context ctx)->{
         String nome = ctx.formParam("nome");
     };
->>>>>>> d0ed33ceb067f9def86e1b334fb5ba9d982bd996
 }
